e6b2bbfa8917056deb49e49a860b6bdb49085293
Shatoparba Banerjee <shatoparbabanerjee@Shatoparbas-MacBook-Air.local>
2023-11-02 20:54:31 -0700

    i will punch something

--- src/tm_lif.v
+++ src/tm_lif.v
@@ -11,29 +11,25 @@ module tm_lif (
     wire [7:0] threshold [7:0];
     wire [7:0] next_state [7:0];
     reg [2:0] tm_counter;
+    
+    integer i; // Declare i outside the loop
 
-    always @(posedge clk) begin
+    always @(posedge clk or negedge rst_n) begin
         if (!rst_n) begin
-            for (int i = 0; i < 8; i = i + 1) begin
+            for (i = 0; i < 8; i = i + 1) begin
                 state[i] <= 0;
-                threshold[i] <= 127;
+                threshold[i] <= 8'b01111111; // Correct threshold initialization
                 next_state[i] <= 0;
             end
             tm_counter <= 0;
         end else begin
-            for (int i = 0; i < 8; i = i + 1) begin
+            for (i = 0; i < 8; i = i + 1) begin
                 if (tm_counter == i) begin
-                    next_state[i] <= state[i]; 
+                    next_state[i] <= state[i] + (current >> 1); // Fixed state update equation
                 end
                 spike[i] <= (next_state[i] >= threshold[i]);
             end
-            tm_counter <= tm_counter + 1;
-        end
-    end
-
-    always @* begin
-        for (int i = 0; i < 8; i = i + 1) begin
-            next_state[i] = current + (state[i] >> 1);
+            tm_counter <= (tm_counter + 1) % 8; // Updated counter
         end
     end
 
