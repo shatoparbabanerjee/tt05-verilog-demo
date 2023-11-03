88c6557a673ce931ff9a7a7cb02a50a8653ca0fb
Shatoparba Banerjee <shatoparbabanerjee@eduroam-169-233-194-249.ucsc.edu>
2023-11-03 11:54:48 -0700

    CBLIF multiple neurons

--- src/tt_um_lif.v
+++ src/tt_um_lif.v
@@ -17,7 +17,7 @@ module tt_um_lif (
 
     // Instantiate the segment display module
     lif lif1(.current(ui_in), .clk(clk), .rst_n(rst_n), .spike(uio_out[7]), .state(uo_out));
-    //lif lif2(.current({uio_out[7], 7'b0000000}), .clk(clk), .rst_n(rst_n), .spike(uio_out[6]), .state(uo_out));
+    lif lif2(.current({uio_out[7], 7'b0000000}), .clk(clk), .rst_n(rst_n), .spike(uio_out[6]), .state(uo_out));
     //current_based_lif lif1(.input_current(ui_in), .clk(clk), .rst_n(rst_n), .spike(uio_out[7]), .state(next_state));
 
     // Connect the output state from the current-based LIF neuron to the 7-segment display output
