0f7855482f3d8af97f0c4296d7311cc138a76f63
Shatoparba Banerjee <shatoparbabanerjee@Shatoparbas-MacBook-Air.local>
2023-11-02 23:55:42 -0700

    LIF_final adding testbench

--- src/lif.v
+++ src/lif.v
@@ -1,27 +1,26 @@
 `default_nettype none
 
 module lif (
-    input wire [7:0] current,
-    input wire clk,
-    input wire rst_n,
-    output wire spike,
-    output reg [7:0] state
+    input wire [7:0] current,   // Input current, 8 bits wide
+    input wire clk,             // Input clock signal
+    input wire rst_n,           // Input reset signal (active low)
+    output wire spike,          // Output spike signal
+    output reg [7:0] state      // Output state register, 8 bits wide
 );
 
-    reg [7:0] threshold;  
-    wire [7:0] next_state;
+    reg [7:0] threshold;        // Internal register for threshold value
+    wire [7:0] next_state;      // Wire to compute the next state value
 
     always @(posedge clk) begin
         if (!rst_n) begin
-            state <= 0;
-            threshold <= 127;
+            state <= 0;          // Reset the state to 0 when reset is active
+            threshold <= 127;    // Set the threshold value to 127 during reset
         end else begin
-            state <= next_state;
+            state <= next_state; // Update the state with the next_state value
         end
     end
 
-    assign next_state = current + (state >> 1);
-    assign spike = (state >= threshold);
-    
+    assign next_state = current + (state >> 1);   // Compute the next state as the sum of current and half of the current state
+    assign spike = (state >= threshold);           // Determine if a spike should occur based on the state and threshold
 
 endmodule
--- src/tt_um_lif.v
+++ src/tt_um_lif.v
@@ -2,21 +2,20 @@
 
 module tt_um_lif (
     input  wire [7:0] ui_in,    // Dedicated inputs - connected to the input switches
-    output wire [7:0] uo_out,   // Dedicated outputs - connected to the 7 segment display
+    output wire [7:0] uo_out,   // Dedicated outputs - connected to the 7-segment display
     input  wire [7:0] uio_in,   // IOs: Bidirectional Input path
     output wire [7:0] uio_out,  // IOs: Bidirectional Output path
     output wire [7:0] uio_oe,   // IOs: Bidirectional Enable path (active high: 0=input, 1=output)
-    input  wire       ena,      // will go high when the design is enabled
-    input  wire       clk,      // clock
-    input  wire       rst_n     // reset_n - low to reset
+    input  wire       ena,      // Input enable signal, active high
+    input  wire       clk,      // Input clock signal
+    input  wire       rst_n     // Input reset signal (active low)
 );
 
-    assign uio_oe = 8'b11111111;
-    assign uio_out[6:0] = 7'd0;
+    assign uio_oe = 8'b11111111;      // Set all bits of the bidirectional enable path to 1, indicating they are in output mode
+    assign uio_out[6:0] = 7'd0;       // Initialize the 7-segment display output to all zeros
 
-
-    // instantiate segment display
-   lif lif1(.current(ui_in), .clk(clk), .rst_n(rst_n), .spike(uio_out[7]), .state(uo_out));
-   //lif lif2(.current({uio_out[7], 7'b0000000}), .clk(clk), .rst_n(rst_n), .spike(uio_out[6]), .state(uo_out));
+    // Instantiate the segment display module
+    lif lif1(.current(ui_in), .clk(clk), .rst_n(rst_n), .spike(uio_out[7]), .state(uo_out));
+    //lif lif2(.current({uio_out[7], 7'b0000000}), .clk(clk), .rst_n(rst_n), .spike(uio_out[6]), .state(uo_out));
 
 endmodule
