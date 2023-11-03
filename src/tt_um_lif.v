66d0475161f7b3e59311cbed47df02d5236c3e2d
Shatoparba Banerjee <shatoparbabanerjee@eduroam-169-233-194-249.ucsc.edu>
2023-11-03 13:12:41 -0700

    LIF multiple neuron spike 4

--- src/tt_um_lif.v
+++ src/tt_um_lif.v
@@ -14,9 +14,9 @@ module tt_um_lif (
     assign uio_oe = 8'b11111111;      // Set all bits of the bidirectional enable path to 1, indicating they are in output mode
     assign uio_out[4:0] = 5'd0;       // Initialize the 7-segment display output to all zeros
 
-    wire state_signals[7:0]; // another array
-    wire state_signals2[7:0];
-    wire state_signals3[7:0];
+    reg [7:0] state_signals;
+    reg [7:0] state_signals2;
+    reg [7:0] state_signals3;
 
     assign uo_out[7:0] = state_signals[7:0];  
 
