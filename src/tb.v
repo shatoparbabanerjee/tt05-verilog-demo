8f8fc92540c46262f869d99b4f775ae47e379c12
Shatoparba Banerjee <shatoparbabanerjee@Shatoparbas-MacBook-Air.local>
2023-11-02 23:49:03 -0700

    LIF_final adding testbench

--- src/tb.v
+++ src/tb.v
@@ -46,32 +46,6 @@ that can be driven / tested by the cocotb test.py
 
 // endmodule
 
-// Include the LIF module
-module lif (
-    input wire [7:0] current,
-    input wire clk,
-    input wire rst_n,
-    output wire spike,
-    output reg [7:0] state
-);
-
-    reg [7:0] threshold;
-    wire [7:0] next_state;
-
-    always @(posedge clk) begin
-        if (!rst_n) begin
-            state <= 8'h00;
-            threshold <= 8'h7F;
-        end else begin
-            state <= next_state;
-        end
-    end
-
-    assign next_state = current + (state >> 1);
-    assign spike = (state >= threshold);
-
-endmodule
-
 module tb ();
 
     // Dump the trace to a VCD file for waveform viewing
