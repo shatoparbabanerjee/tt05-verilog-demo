b1f3411f4d25b95163436ed3962e78e89a5be97a
Shatoparba Banerjee <shatoparbabanerjee@Shatoparbas-MacBook-Air.local>
2023-11-02 23:25:49 -0700

    pt 2 testing current based code

--- src/tb.v
+++ src/tb.v
@@ -28,50 +28,20 @@ module tb ();
     wire [7:0] uio_out;
     wire [7:0] uio_oe;
 
-    initial begin
-        // Initialize inputs
-        clk = 0;
-        rst_n = 1;
-        ena = 1;
-        ui_in = 8'h00;
-        uio_in = 8'h00;
-
-        // Stimulus generation
-        #5 clk = 1;
-        forever #5 clk = ~clk;
-
-        // Call the LIF module
-        current_based_tt_um_lif lif_instance (
-            .ui_in(ui_in),
-            .uo_out(uo_out),
-            .uio_in(uio_in),
-            .uio_out(uio_out),
-            .uio_oe(uio_oe),
-            .ena(ena),
-            .clk(clk),
-            .rst_n(rst_n)
+    current_based_tt_um_lif current_based_tt_um_lif (
+    // include power ports for the Gate Level test
+    `ifdef GL_TEST
+        .VPWR( 1'b1),
+        .VGND( 1'b0),
+    `endif
+        .ui_in      (ui_in),    // Dedicated inputs
+        .uo_out     (uo_out),   // Dedicated outputs
+        .uio_in     (uio_in),   // IOs: Input path
+        .uio_out    (uio_out),  // IOs: Output path
+        .uio_oe     (uio_oe),   // IOs: Enable path (active high: 0=input, 1=output)
+        .ena        (ena),      // enable - goes high when design is selected
+        .clk        (clk),      // clock
+        .rst_n      (rst_n)     // not reset
         );
-
-        // Monitor for spikes
-        always @(posedge clk) begin
-            if (uo_out[7]) $display("Spike detected!");
-        end
-    end
-
-    // tt_um_lif tt_um_lif (
-    // // include power ports for the Gate Level test
-    // `ifdef GL_TEST
-    //     .VPWR( 1'b1),
-    //     .VGND( 1'b0),
-    // `endif
-    //     .ui_in      (ui_in),    // Dedicated inputs
-    //     .uo_out     (uo_out),   // Dedicated outputs
-    //     .uio_in     (uio_in),   // IOs: Input path
-    //     .uio_out    (uio_out),  // IOs: Output path
-    //     .uio_oe     (uio_oe),   // IOs: Enable path (active high: 0=input, 1=output)
-    //     .ena        (ena),      // enable - goes high when design is selected
-    //     .clk        (clk),      // clock
-    //     .rst_n      (rst_n)     // not reset
-    //     );
 
 endmodule
