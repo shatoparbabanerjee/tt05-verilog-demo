e9356ac07a0bb4562b3543e70f96e372a12c4b24
Shatoparba Banerjee <shatoparbabanerjee@Shatoparbas-MacBook-Air.local>
2023-11-02 19:19:41 -0700

    editing tm_lif agn

--- src/tm_lif.v
+++ src/tm_lif.v
@@ -9,7 +9,8 @@ module tm_lif (
 );
 
     reg [7:0] threshold [7:0];
-    wire [7:0] next_state [7:0];
+    reg [7:0] next_state [7:0];
+    reg [7:0] state [7:0];
     reg [2:0] tm_counter;
 
     always @(posedge clk) begin
@@ -31,21 +32,21 @@ module tm_lif (
         end
     end
 
-    assign next_state[0] = current + (state >> 1);
-    assign spike[0] = (state >= threshold[0]);
-    assign next_state[1] = current + (state >> 1);
-    assign spike[1] = (state >= threshold[1]);
-    assign next_state[2] = current + (state >> 1);
-    assign spike[2] = (state >= threshold[2]);
-    assign next_state[3] = current + (state >> 1);
-    assign spike[3] = (state >= threshold[3]);
-    assign next_state[4] = current + (state >> 1);
-    assign spike[4] = (state >= threshold[4]);
-    assign next_state[5] = current + (state >> 1);
-    assign spike[5] = (state >= threshold[5]);
-    assign next_state[6] = current + (state >> 1);
-    assign spike[6] = (state >= threshold[6]);
-    assign next_state[7] = current + (state >> 1);
-    assign spike[7] = (state >= threshold[7]);
+    assign next_state[0] = current + (state[0] >> 1);
+    assign spike[0] = (state[0] >= threshold[0]);
+    assign next_state[1] = current + (state[1] >> 1);
+    assign spike[1] = (state[1] >= threshold[1]);
+    assign next_state[2] = current + (state[2] >> 1);
+    assign spike[2] = (state[2] >= threshold[2]);
+    assign next_state[3] = current + (state[3] >> 1);
+    assign spike[3] = (state[3] >= threshold[3]);
+    assign next_state[4] = current + (state[4] >> 1);
+    assign spike[4] = (state[4] >= threshold[4]);
+    assign next_state[5] = current + (state[5] >> 1);
+    assign spike[5] = (state[5] >= threshold[5]);
+    assign next_state[6] = current + (state[6] >> 1);
+    assign spike[6] = (state[6] >= threshold[6]);
+    assign next_state[7] = current + (state[7] >> 1);
+    assign spike[7] = (state[7] >= threshold[7]);
 
 endmodule
