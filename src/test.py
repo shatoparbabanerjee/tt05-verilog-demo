0a688d6eeaec104fab011d176f3fbaf65279df06
Shatoparba Banerjee <shatoparbabanerjee@Shatoparbas-MacBook-Air.local>
2023-11-16 23:21:02 -0800

    CBLIF Updated for Report 2

--- src/test.py
+++ src/test.py
@@ -1,3 +1,30 @@
+# import cocotb
+# from cocotb.clock import Clock
+# from cocotb.triggers import RisingEdge, FallingEdge, Timer, ClockCycles
+
+# @cocotb.test()
+# async def test_my_design(dut):
+#     CONSTANT_CURRENT = 80
+
+#     dut._log.info("Start Simulation")
+
+#     #init clock 
+#     clock = Clock(dut.clk, 1, units="ns")
+#     cocotb.start_soon(clock.start())
+
+#     dut.rst_n.value = 0
+#     await ClockCycles(dut.clk, 10)
+#     dut.rst_n.value = 1 
+
+#     dut.ui_in.value = CONSTANT_CURRENT
+#     dut.ena.value = 1 
+
+#     for i in range(200):
+#         await RisingEdge(dut.clk)
+    
+#     assert dut.ui_in.value == dut.ui_in.value
+#     dut._log.info("Finished Test")
+
 import cocotb
 from cocotb.clock import Clock
 from cocotb.triggers import RisingEdge, FallingEdge, Timer, ClockCycles
@@ -5,6 +32,8 @@ from cocotb.triggers import RisingEdge, FallingEdge, Timer, ClockCycles
 @cocotb.test()
 async def test_my_design(dut):
     CONSTANT_CURRENT = 80
+    NEW_CONSTANT_CURRENT = 100
+    SUB_THRESHOLD_CURRENT = 50
 
     dut._log.info("Start Simulation")
 
@@ -29,13 +58,13 @@ async def test_my_design(dut):
     await ClockCycles(dut.clk, 10)
     dut.rst_n.value = 1 
 
-    dut.ui_in.value = 100  # Input a different constant synaptic current
+    dut.ui_in.value = NEW_CONSTANT_CURRENT  # Input a different constant synaptic current
     dut.ena.value = 1 
 
     for i in range(200):
         await RisingEdge(dut.clk)
     
-    assert dut.ui_in.value == 100
+    assert dut.ui_in.value == NEW_CONSTANT_CURRENT
     assert dut.spike.value == 1  # Verify firing behavior after reset and re-input
 
     # Test Scenario 3: No Firing
@@ -43,13 +72,14 @@ async def test_my_design(dut):
     await ClockCycles(dut.clk, 10)
     dut.rst_n.value = 1 
 
-    dut.ui_in.value = 50  # Input a sub-threshold synaptic current
+    dut.ui_in.value = SUB_THRESHOLD_CURRENT  # Input a sub-threshold synaptic current
     dut.ena.value = 1 
 
     for i in range(200):
         await RisingEdge(dut.clk)
     
-    assert dut.ui_in.value == 50
+    assert dut.ui_in.value == SUB_THRESHOLD_CURRENT
     assert dut.spike.value == 0  # Confirm that the neuron does not fire
 
     dut._log.info("Finished Test Scenarios")
+
