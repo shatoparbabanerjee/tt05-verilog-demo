8f7e04e171d04299d790fcaf9f2d7ef3f850b5a2
Shatoparba Banerjee <shatoparbabanerjee@Shatoparbas-MacBook-Air.local>
2023-11-16 23:15:49 -0800

    CBLIF Updated for Report

--- src/test.py
+++ src/test.py
@@ -8,10 +8,7 @@ async def test_my_design(dut):
 
     dut._log.info("Start Simulation")
 
-    #init clock 
-    clock = Clock(dut.clk, 1, units="ns")
-    cocotb.start_soon(clock.start())
-
+    # Test Scenario 1: Normal Firing
     dut.rst_n.value = 0
     await ClockCycles(dut.clk, 10)
     dut.rst_n.value = 1 
@@ -23,4 +20,36 @@ async def test_my_design(dut):
         await RisingEdge(dut.clk)
     
     assert dut.ui_in.value == dut.ui_in.value
-    dut._log.info("Finished Test")
+    assert dut.spike.value == 1  # Verify that the neuron fires
+    await Timer(10, units='ns')  # Wait for refractory period
+    assert dut.spike.value == 0  # Confirm proper operation of the refractory period
+
+    # Test Scenario 2: Reset and Re-Input
+    dut.rst_n.value = 0
+    await ClockCycles(dut.clk, 10)
+    dut.rst_n.value = 1 
+
+    dut.ui_in.value = 100  # Input a different constant synaptic current
+    dut.ena.value = 1 
+
+    for i in range(200):
+        await RisingEdge(dut.clk)
+    
+    assert dut.ui_in.value == 100
+    assert dut.spike.value == 1  # Verify firing behavior after reset and re-input
+
+    # Test Scenario 3: No Firing
+    dut.rst_n.value = 0
+    await ClockCycles(dut.clk, 10)
+    dut.rst_n.value = 1 
+
+    dut.ui_in.value = 50  # Input a sub-threshold synaptic current
+    dut.ena.value = 1 
+
+    for i in range(200):
+        await RisingEdge(dut.clk)
+    
+    assert dut.ui_in.value == 50
+    assert dut.spike.value == 0  # Confirm that the neuron does not fire
+
+    dut._log.info("Finished Test Scenarios")
